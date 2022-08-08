5be3328d1482a5517eb928b17b7cff039899307b
Bruno Godinho <godinho_bruno@hotmail.com>
2022-08-08 11:41:37 +0200

    main page links working

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,11 +1,13 @@
 import './category-item.styles.scss'
+import { useNavigate } from 'react-router-dom';
 
 const CategoryItem = ({category}) =>{
-  const {imageUrl,title } = category;
+  const {imageUrl,title,route } = category;
+  const navigate = useNavigate();
+  const onNavigateHandler = ()=> navigate(route)
 
   return(
-
-    <div className="directory-item-container">
+    <div className="directory-item-container" onClick={onNavigateHandler}>
       <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}}></div>
       <div className="directory-item-body">
         <h2>{title}</h2>
