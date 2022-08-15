ad803d941652e23316d0f3001ca64ea3c57bd71e
Bruno Godinho <godinho_bruno@hotmail.com>
2022-08-15 09:15:43 +0200

    working on redux

--- src/contexts/cart-context.jsx
+++ src/contexts/cart-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
 
 const addCartItem = (cartItems,productToAdd)=>{
   const existingCartItem = cartItems.find(cartItem=>cartItem.id === productToAdd.id)
@@ -37,36 +37,72 @@ export const CartContext = createContext({
   total:0
 })
 
-export const CartProvider = ({children})=>{
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const [cartCount , setCartCount] = useState(0);
-  const [cartTotal, setCartTotal] = useState(0);
+const INITIAL_STATE = {
+  isCartOpen: true,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0
+}
 
-  useEffect(()=>{
-    const newCartCount = cartItems.reduce((total ,item)=>{
+const CART_ACTION_OPTIONS = {
+  SET_CART_ITEMS: 'SET_CART_ITEMS',
+  SET_IS_CART_OPEN: 'SET_IS_CART_OPEN'
+}
+
+const cartReducer = (state,action) =>{
+  const { type, payload } = action;
+  switch (type) {
+    case CART_ACTION_OPTIONS.SET_CART_ITEMS:
+      return {
+        ...state,
+        ...payload
+      }
+    case CART_ACTION_OPTIONS.SET_IS_CART_OPEN:
+      return{
+        ...state,
+        isCartOpen: !state.isCartOpen
+      }
+    default:
+      throw new Error ('unhandled reducer type')
+  }
+}
+
+export const CartProvider = ({children})=>{
+  const [{isCartOpen, cartItems, cartCount, cartTotal}, dispatch] =useReducer(cartReducer,INITIAL_STATE);
+
+  const updateCartItemsReducer = (newCartItems) =>{
+    const newCartCount = newCartItems.reduce((total ,item)=>{
       return total + item.quantity
     },0)
-    setCartCount(newCartCount)
-  },[cartItems])
 
-  useEffect(()=>{
-    const newCartTotal = cartItems.reduce((total ,item)=>{
+    const newCartTotal = newCartItems.reduce((total ,item)=>{
       return total + item.quantity * item.price
     },0)
-    setCartTotal(newCartTotal)
-  },[cartItems])
+
+    dispatch({type:"SET_CART_ITEMS",payload:{
+      cartItems:newCartItems,
+      cartTotal: newCartTotal,
+      cartCount: newCartCount
+    }})
+  }
+
+  const setIsCartOpen = () =>{
+    dispatch({type:"SET_IS_CART_OPEN"})
+  }
 
   const addItemToCart = (productToAdd)=>{
-    setCartItems(addCartItem(cartItems,productToAdd))
+    const newCartItems = addCartItem(cartItems,productToAdd)
+    updateCartItemsReducer(newCartItems)
   }
 
   const RemoveItemFromCart = (cartItemToRemove)=>{
-    setCartItems(removeCartItem(cartItems,cartItemToRemove))
+    const newCartItems = removeCartItem(cartItems,cartItemToRemove)
+    updateCartItemsReducer(newCartItems)
   }
 
   const clearItemFromCart = (cartItemToClear) =>{
-    setCartItems(clearCartItem(cartItems,cartItemToClear))
+    const newCartItems = clearCartItem(cartItems,cartItemToClear)
+    updateCartItemsReducer(newCartItems)
   }
 
   const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount, RemoveItemFromCart, clearItemFromCart,cartTotal }
---
+++ src/store/store.js
@@ -0,0 +1,2 @@
+import { compose, createStore, applyMiddleWare } from "redux";
+import logger from 'redux-logger'
